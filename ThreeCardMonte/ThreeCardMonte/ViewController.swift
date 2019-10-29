ee7708fa9acb20c54d3bbffa6cd81b5c77c0c6ce
Tiffany Obi <tiffanyobi123@gmail.com>
2019-10-29 17:06:40 -0400

    fixed the app to set image back to redCard when new game starts added graphics added win or lose message

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,85 +9,97 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
     
-        
-        @IBOutlet weak var resultOfGame: UILabel!
-        
-        
-        @IBOutlet weak var tag0: UIButton!
-        
-        @IBOutlet weak var tag1: UIButton!
-        
-        
-        @IBOutlet weak var tag2: UIButton!
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
     
-
-
-        @IBAction func flipCardsOver(_ sender: UIButton) {
-            
-           let arrayOfTags = [tag0,tag1,tag2]
-            
-            let randomImages = ["threeCard","threeCard","kingCard"]
-            
     
+    
+    
+    @IBOutlet weak var resultOfGame: UILabel!
+    
+    
+    @IBOutlet weak var tag0: UIButton!
+    
+    @IBOutlet weak var tag1: UIButton!
+    
+    
+    @IBOutlet weak var tag2: UIButton!
+    
+    
+    
+    
+    @IBAction func flipCardsOver(_ sender: UIButton) {
+        
+        let arrayOfTags = [tag0,tag1,tag2]
+        
+        let imagesArray = ["threeCard","threeCard","kingCard"]
+        
+        let randomImages = imagesArray.randomElement() ?? "threeCard"
+        
+        switch sender.tag {
             
-            switch sender.tag {
-            
-            case 0:
-                sender.setBackgroundImage(UIImage(named:"\( randomImages.randomElement() ?? "threeCard")"), for: UIControl.State.normal )
-                
-                for tag in arrayOfTags {
-                    tag?.isEnabled = false
-                }
-                
-            case 1:
-                sender.setBackgroundImage(UIImage(named:"\( randomImages.randomElement() ?? "kingCard")"), for: UIControl.State.normal )
-                
-                for tag in arrayOfTags {
-                    tag?.isEnabled = false
-                           }
-                
-            case 2:
-                sender.setBackgroundImage(UIImage(named:"\( randomImages.randomElement() ?? "threeCard")"), for: UIControl.State.normal )
-                
-                for tag in arrayOfTags {
-                        tag?.isEnabled = false
-                           }
-                
-            default:
-                sender.setBackgroundImage(UIImage(named:"\( randomImages.randomElement() ?? "threeCard")"), for: UIControl.State.normal )
-                
-                
-                for tag in arrayOfTags {
-                    tag?.isEnabled = false
-                           }
-                
-            }
-        }
-        
-        
-        
-        @IBAction func startNewGame(_ sender: UIButton) {
-            let arrayOfTags = [tag0,tag1,tag2]
+        case 0:
+            sender.setBackgroundImage(UIImage(named:"\(randomImages)"), for: UIControl.State.normal )
             
             for tag in arrayOfTags {
-            tag?.isEnabled = true
-                   
+                tag?.isEnabled = false
+            }
+            if randomImages == "kingCard"
+            {
+                resultOfGame.text = "YOU WIN !!! "
+            } else { resultOfGame.text = "Sorry, You Lose!"}
+            
+        case 1:
+            sender.setBackgroundImage(UIImage(named:"\(randomImages)"), for: UIControl.State.normal )
+            
+            for tag in arrayOfTags {
+                tag?.isEnabled = false
+            }
+            if randomImages == "kingCard"
+            {
+                resultOfGame.text = "YOU WIN !!! "
+            } else { resultOfGame.text = "Sorry, You Lose!"}
+        case 2:
+            sender.setBackgroundImage(UIImage(named:"\(randomImages)"), for: UIControl.State.normal )
+            
+            for tag in arrayOfTags {
+                tag?.isEnabled = false
+            }
+            if randomImages == "kingCard"
+            {
+                resultOfGame.text = "YOU WIN !!! "
+            } else { resultOfGame.text = "Sorry, You Lose!"}
+        default:
+            sender.setBackgroundImage(UIImage(named:"\(randomImages)"), for: UIControl.State.normal )
+            
+            for tag in arrayOfTags {
+                tag?.isEnabled = false
+                
+            }
+            
         }
-            arrayOfTags.forEach({$0?.setImage(UIImage(named: "cardBackRed"), for: .normal)})
-        
         
     }
+    
+    
+    @IBAction func startNewGame(_ newGameButton: UIButton) {
         
+        let arrayOfTags = [tag0,tag1,tag2]
         
+        resultOfGame.text = "Pick A Card"
+        
+        for tag in arrayOfTags {
+            tag?.isEnabled = true
+        }
+        
+        tag0.setBackgroundImage(UIImage(named: "cardBackRed"), for: UIControl.State.normal)
+        tag1.setBackgroundImage(UIImage(named: "cardBackRed"), for: UIControl.State.normal)
+        tag2.setBackgroundImage(UIImage(named: "cardBackRed"), for: UIControl.State.normal)
         
     }
-
-
+    
+}
 
